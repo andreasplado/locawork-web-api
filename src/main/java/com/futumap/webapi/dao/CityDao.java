3aaecd32091a64d78ade748d575eeeb94f71d5d0
Andreas Plado <andreasplado@gmail.com>
2019-10-05 19:46:18 +0300

    Added joda dependency

---
+++ src/main/java/com/futumap/webapi/dao/CityDao.java
@@ -0,0 +1,4 @@
+package com.futumap.webapi.dao;
+
+public class CityDao {
+}
---
+++ src/main/java/com/futumap/webapi/dao/entity/CityEntity.java
@@ -0,0 +1,94 @@
+package com.futumap.webapi.dao.entity;
+
+import org.joda.time.DateTime;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "city")
+public class CityEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "population")
+    private Long population;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private Date updatedAt;
+
+    public CityEntity() {
+
+    }
+
+    public CityEntity(String name, Long population) {
+        this.setName(name);
+        this.setPopulation(population);
+        this.setCreatedAt(new Date());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Long population) {
+        this.population = population;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void prePersist() {
+        if (this.createdAt == null) createdAt = new Date();
+        if (this.updatedAt == null) updatedAt = new Date();
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        this.updatedAt = new Date();
+    }
+
+    @PreRemove
+    protected void preRemove() {
+        this.updatedAt = new Date();
+    }
+}
+
