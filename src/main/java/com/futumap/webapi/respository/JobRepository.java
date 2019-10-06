698b72c09318e1138d0288215c3e82ba2275996d
Andreas Plado <andreasplado@gmail.com>
2019-10-07 00:44:14 +0300

    SQL Query

--- src/main/java/com/futumap/webapi/respository/JobRepository.java
+++ src/main/java/com/futumap/webapi/respository/JobRepository.java
@@ -12,6 +12,9 @@ import java.util.List;
 @Repository
 public interface JobRepository extends JpaRepository<JobEntity, Integer> {
 
-    @Query(value="SELECT * FROM jobs WHERE ST_DWithin(location, ST_SetSRID(ST_MakePoint(?1, ?2), 3785), ?3);", nativeQuery = true)
+    @Query(value="SELECT *, point(?1, ?2) <@> point(longitude, latitude)::point as distance \n" +
+            "FROM location\n" +
+            "WHERE (point(?1, ?2) <@> point(longitude, latitude)) < 10\n" +
+            "ORDER BY distance;", nativeQuery = true)
     List<JobEntity> findNearestJobs(@Param("longitude") String longitude, @Param("latitude") String latitude, @Param("distance") String distance);
 }
