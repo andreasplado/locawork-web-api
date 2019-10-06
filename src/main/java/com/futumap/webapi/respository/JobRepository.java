daac99340a195ff29acbd28888ca5b7565d0d076
Andreas Plado <andreasplado@gmail.com>
2019-10-06 21:00:11 +0300

    Request param

--- src/main/java/com/futumap/webapi/respository/JobRepository.java
+++ src/main/java/com/futumap/webapi/respository/JobRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -15,5 +16,5 @@ public interface JobRepository extends JpaRepository<JobEntity, Integer> {
     @Query(value="SELECT * \n" +
             "FROM jobs \n" +
             "WHERE ST_Dwithin(geom, st_point(?1, ?2,?3);", nativeQuery = true)
-    List<JobEntity> findNearestJobs(@Param("longitude") String longitude, @Param("latitude") String latitude, @Param("distance") String distance);
+    List<JobEntity> findNearestJobs(@RequestParam("longitude") String longitude, @RequestParam("latitude") String latitude, @RequestParam("distance") String distance);
 }
