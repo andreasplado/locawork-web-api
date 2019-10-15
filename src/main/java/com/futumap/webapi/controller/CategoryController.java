274c292f8394fb88a7ee9bc652e26c8e54dc3c68
Andreas Plado <andreasplado@gmail.com>
2019-10-15 16:33:54 +0300

    Changed endpoint

--- src/main/java/com/futumap/webapi/controller/CategoryController.java
+++ src/main/java/com/futumap/webapi/controller/CategoryController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.util.UriComponentsBuilder;
 import java.util.Optional;
 
-@RequestMapping("/jobcategories")
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
