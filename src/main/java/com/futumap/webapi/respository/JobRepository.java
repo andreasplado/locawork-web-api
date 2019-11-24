dec9e20096119da2cf4deea56601122bfbf8c818
Andreas Plado <andreasplado@gmail.com>
2019-11-24 19:12:10 +0200

    Refactoring

--- src/main/java/com/futumap/webapi/respository/JobRepository.java
+++ src/main/java/com/futumap/webapi/respository/JobRepository.java
@@ -14,12 +14,12 @@ import java.util.List;
 @Repository
 public interface JobRepository extends JpaRepository<JobEntity, Integer> {
 
-    @Query(value="SELECT * from jobs j WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND j.account_google_id !=?4", nativeQuery = true)
+    @Query(value="SELECT * from jobs j WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND j.account_google_id !=?4 AND regitred_account IS NULL", nativeQuery = true)
     List<JobEntity> findNearestJobs(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("distance") Double distance, @Param("googleAccount") String accountGoogleId);
 
     @Query(value="SELECT * from jobs j WHERE account_google_id=?1", nativeQuery = true)
     List<JobEntity> findPostedJobs(@Param("googleAccount") String accountGoogleId);
 
-    @Query(value="SELECT * from jobs j WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude)", nativeQuery = true)
+    @Query(value="SELECT * from jobs j WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND regitred_account IS NULL", nativeQuery = true)
     List<JobEntity> findAllNearestJobs(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("distance") Double distance);
 }
