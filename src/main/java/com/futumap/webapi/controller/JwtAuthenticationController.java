7629fd41b25a38fa4e47eefd3edc7ff6c3143b6b
Andreas Plado <andreasplado@gmail.com>
2020-09-02 20:57:33 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/controller/JwtAuthenticationController.java
+++ src/main/java/com/futumap/webapi/controller/JwtAuthenticationController.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationController {
 
     @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
     public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-        authenticate(authenticationRequest.getUsername(), authenticationRequest.getGoogleAccountId());
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getUsername());
         final UserDetails userDetails = userService
                 .findByName(authenticationRequest.getUsername());
         final String token = jwtTokenUtil.generateToken(userDetails);
