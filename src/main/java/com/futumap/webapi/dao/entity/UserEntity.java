95653ddadfcea9ebd0cd3b16e1111c72f2e4d1e0
Andreas Plado <andreasplado@gmail.com>
2020-09-03 01:50:37 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/dao/entity/UserEntity.java
+++ src/main/java/com/futumap/webapi/dao/entity/UserEntity.java
@@ -107,17 +107,17 @@ public class UserEntity implements UserDetails {
 
     @Override
     public boolean isAccountNonExpired() {
-        return this.isAccountNonExpired;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return this.isAccountNonLocked;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return this.isCredentialsNonExpired;
+        return true;
     }
 
     @Override
