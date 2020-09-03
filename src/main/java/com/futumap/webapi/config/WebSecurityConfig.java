0b61d6f19a99ea5137a74b4df83113ef3ba670b1
Andreas Plado <andreasplado@gmail.com>
2020-09-03 07:16:38 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/config/WebSecurityConfig.java
+++ src/main/java/com/futumap/webapi/config/WebSecurityConfig.java
@@ -1,24 +1,18 @@
 package com.futumap.webapi.config;
 
-import com.futumap.webapi.dao.entity.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -28,17 +22,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     @Autowired
-    private UserDetailsService userDetailsService;
+    private UserDetailsService jwtUserDetailsService;
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
-        auth.parentAuthenticationManager(authenticationManagerBean());
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+// configure AuthenticationManager so that it knows from where to load
+// user for matching credentials
+// Use BCryptPasswordEncoder
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Bean
@@ -46,35 +39,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
     @Bean
+    @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return s -> new UserEntity();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/**");
-    }
-
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        // We don't need CSRF for this example
+// We don't need CSRF for this example
         httpSecurity.csrf().disable()
-        // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/register","/authenticate").permitAll().
-        // all other requests need to be authenticated
+// dont authenticate this particular request
+                .authorizeRequests().antMatchers("/authenticate").permitAll().
+// all other requests need to be authenticated
         anyRequest().authenticated().and().
-        // make sure we use stateless session; session won't be used to
-        // store user's state.
+// make sure we use stateless session; session won't be used to
+// store user's state.
         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        // Add a filter to validate the tokens with every request
+// Add a filter to validate the tokens with every request
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
--- src/main/java/com/futumap/webapi/controller/JwtAuthenticationController.java
+++ src/main/java/com/futumap/webapi/controller/JwtAuthenticationController.java
@@ -7,21 +7,18 @@ import com.futumap.webapi.dao.entity.UserEntity;
 import com.futumap.webapi.model.ResponseModel;
 import com.futumap.webapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Collection;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
@@ -43,31 +40,20 @@ public class JwtAuthenticationController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public ResponseEntity<?> register(@RequestBody UserEntity userEntity)  {
+    public ResponseEntity<?> register(@RequestBody UserEntity userEntity) throws Exception {
         if(userService.existsByUsername(userEntity.getUsername())){
-            ResponseModel responseModel = new ResponseModel();
-            responseModel.setMessage("User already exists!");
-
+            ResponseModel responseModel= new ResponseModel();
+            responseModel.setMessage("Username already exists!");
             return ResponseEntity.ok(responseModel);
         }else{
-            BCryptPasswordEncoder pbkdf2PasswordEncoder = new BCryptPasswordEncoder();
-            String encodedPass = pbkdf2PasswordEncoder.encode(userEntity.getPassword());
-
-            userEntity.setPassword(encodedPass);
             userService.save(userEntity);
-
             return ResponseEntity.ok(userEntity);
         }
     }
 
     private void authenticate(String username, String password) throws Exception {
         try {
-            BCryptPasswordEncoder pbkdf2PasswordEncoder = new BCryptPasswordEncoder();
-            String encodedPass = pbkdf2PasswordEncoder.encode(password);
-            System.out.println("Password:"  + password);
-            System.out.println("Encoded pass" + encodedPass);
-
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, encodedPass));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         } catch (DisabledException e) {
             throw new Exception("USER_DISABLED", e);
         } catch (BadCredentialsException e) {
