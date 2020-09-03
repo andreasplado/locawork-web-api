827deab94c890cefbe0c11e48e32e26efad02a09
Andreas Plado <andreasplado@gmail.com>
2020-09-03 03:56:52 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/config/WebSecurityConfig.java
+++ src/main/java/com/futumap/webapi/config/WebSecurityConfig.java
@@ -57,7 +57,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/register", "/authenticate");
+        web.ignoring().antMatchers("/authenticate","/register");
     }
 
     @Override
