42d92ebdaeccc26aeb294398c935e1bade4fd591
Andreas Plado <andreasplado@gmail.com>
2020-09-03 04:00:24 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/config/JwtRequestFilter.java
+++ src/main/java/com/futumap/webapi/config/JwtRequestFilter.java
@@ -50,7 +50,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             // authentication
             if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
                 UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
+                        userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
                 usernamePasswordAuthenticationToken
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 // After setting the Authentication in the context, we specify
