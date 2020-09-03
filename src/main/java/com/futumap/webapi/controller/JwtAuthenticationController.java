201c64fd2b9d4957ddfe2967bbab716d8b098038
Andreas Plado <andreasplado@gmail.com>
2020-09-03 04:40:44 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/controller/JwtAuthenticationController.java
+++ src/main/java/com/futumap/webapi/controller/JwtAuthenticationController.java
@@ -54,7 +54,7 @@ public class JwtAuthenticationController {
 
     private void authenticate(String username, String password) throws Exception {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, encodedPass));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
         } catch (DisabledException e) {
             throw new Exception("USER_DISABLED", e);
         } catch (BadCredentialsException e) {
