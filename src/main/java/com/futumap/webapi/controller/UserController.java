05b1ca783a740e5b9c46faa9ba23e61cde2e2244
Andreas Plado <andreasplado@gmail.com>
2020-09-08 19:31:04 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/controller/UserController.java
+++ src/main/java/com/futumap/webapi/controller/UserController.java
@@ -32,11 +32,11 @@ public class UserController {
         return ResponseEntity.ok(userEntities);
     }
 
-    @RequestMapping(value = "{account_google_id}", method = RequestMethod.GET)
-    public ResponseEntity<?> getByGoogleAccount(@PathVariable("account_google_id") String accountGoogleId) {
+    @RequestMapping(value = "/check-user", method = RequestMethod.GET)
+    public ResponseEntity<?> getByGoogleAccount(@RequestParam ("account_google_id") String accountGoogleId) {
         UserEntity userEntity = userService.findByGoogleAccount(accountGoogleId);
 
-        if (userEntity != null) {
+        if (userEntity == null) {
             ResponseModel responseModel = new ResponseModel();
             responseModel.setMessage("Users not found!");
             return ResponseEntity.ok(responseModel);
