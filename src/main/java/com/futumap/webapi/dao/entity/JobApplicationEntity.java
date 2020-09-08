c9fc20986cf0561c74c49bea1ae3810a572165fc
Andreas Plado <andreasplado@gmail.com>
2020-09-08 15:06:14 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/dao/entity/JobApplicationEntity.java
+++ src/main/java/com/futumap/webapi/dao/entity/JobApplicationEntity.java
@@ -19,33 +19,9 @@ public class JobApplicationEntity {
     )
     private Integer id;
 
-    @Column(name = "title")
-    private String title;
-
-    @Column(name = "account_google_id")
-    private String accountGoogleId;
-
-    @Column(name = "description")
-    private String description;
-
-    @Column(name = "category_id")
-    private Integer categoryId;
-
-    @Column(name = "salary")
-    private Double salary;
-
-    @Column(name = "job_start_time")
-    private String jobStartTime;
-
-    @Column(name = "job_end_time")
-    private String jobEndTime;
-
-    @Column(name = "latitude")
-    private Double latitude;
-
-
-    @Column(name = "longitude")
-    private Double longitude;
+    @ManyToOne
+    @JoinColumn(name ="fk_job_entity")
+    private JobEntity jobEntity;
 
     @Column(name = "created_at")
     private Date createdAt;
@@ -53,23 +29,11 @@ public class JobApplicationEntity {
     @Column(name = "updated_at", nullable = false)
     private Date updatedAt;
 
-
-    @Column(name = "regitredAccount")
-    private String registredAccount;
-
-    @Column(name = "applyer_contact")
-    private String applyerContact;
-
     public JobApplicationEntity() {
 
     }
 
     public JobApplicationEntity(String title, String description, Double salary, Double longitude, Double latitude) {
-        this.setTitle(title);
-        this.setDescription(description);
-        this.setSalary(salary);
-        this.setLongitude(longitude);
-        this.setLatitude(latitude);
         this.setCreatedAt(new Date());
     }
 
@@ -111,94 +75,6 @@ public class JobApplicationEntity {
     @PreRemove
     protected void preRemove() {
         this.updatedAt = new Date();
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    private void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    private void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    private void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    private void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    private void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public String getAccountGoogleId() {
-        return accountGoogleId;
-    }
-
-    public void setAccountGoogleId(String accountGoogleId) {
-        this.accountGoogleId = accountGoogleId;
-    }
-
-    public String getRegistredAccount() {
-        return registredAccount;
-    }
-
-    public void setRegistredAccount(String registredAccount) {
-        this.registredAccount = registredAccount;
-    }
-
-    public String getJobStartTime() {
-        return jobStartTime;
-    }
-
-    public void setJobStartTime(String jobStartTime) {
-        this.jobStartTime = jobStartTime;
-    }
-
-    public String getJobEndTime() {
-        return jobEndTime;
-    }
-
-    public void setJobEndTime(String jobEndTime) {
-        this.jobEndTime = jobEndTime;
-    }
-
-    public String getApplyerContact() {
-        return applyerContact;
-    }
-
-    public void setApplyerContact(String applyerContact) {
-        this.applyerContact = applyerContact;
     }
 }
 
