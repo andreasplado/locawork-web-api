673709d55f7cf39f91c37787b2a9f5ffe6ce6fd3
Andreas Plado <andreasplado@gmail.com>
2020-09-12 01:54:31 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/dto/JobApplicationDTO.java
+++ src/main/java/com/futumap/webapi/dto/JobApplicationDTO.java
@@ -1,13 +1,31 @@
 package com.futumap.webapi.dto;
 
+import java.util.Date;
+
 public class JobApplicationDTO {
 
     private Integer id;
+    private boolean isApproved;
+
     private Integer userId;
     private String email;
     private String jobTitle;
     private String jobDescription;
     private Double jobSalary;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public JobApplicationDTO(Integer id, boolean isApproved, String jobTitle,
+                             String jobDescription, Double salary, Date createdAt, Date updatedAt) {
+        this.jobDescription = jobDescription;
+        this.jobSalary = salary;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.id = id;
+        this.isApproved = isApproved;
+        this.jobTitle = jobTitle;
+
+    }
 
     public Integer getUserId() {
         return userId;
@@ -47,5 +65,21 @@ public class JobApplicationDTO {
 
     public void setJobSalary(Double jobSalary) {
         this.jobSalary = jobSalary;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
     }
 }
