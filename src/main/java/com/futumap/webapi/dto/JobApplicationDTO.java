065245cfd0828ec54628f376f42605d506a1b34a
Andreas Plado <andreasplado@gmail.com>
2020-09-12 18:58:19 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/dto/JobApplicationDTO.java
+++ src/main/java/com/futumap/webapi/dto/JobApplicationDTO.java
@@ -2,102 +2,16 @@ package com.futumap.webapi.dto;
 
 import java.util.Date;
 
-public class JobApplicationDTO {
+public interface JobApplicationDTO {
 
-    private Integer id;
-    private boolean isApproved;
-    private Integer userId;
-    private String email;
-    private String jobTitle;
-    private String jobDescription;
-    private Double jobSalary;
-    private Date createdAt;
-    private Date updatedAt;
-    private Integer jobId;
-
-    public JobApplicationDTO(Integer id, boolean isApproved, String jobTitle,
-                             String jobDescription, Double salary, Date createdAt, Date updatedAt, Integer jobId, Integer userId) {
-        this.jobDescription = jobDescription;
-        this.jobSalary = salary;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.id = id;
-        this.isApproved = isApproved;
-        this.jobTitle = jobTitle;
-        this.jobId = jobId;
-        this.userId = userId;
-
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public String getJobDescription() {
-        return jobDescription;
-    }
-
-    public void setJobDescription(String jobDescription) {
-        this.jobDescription = jobDescription;
-    }
-
-    public Double getJobSalary() {
-        return jobSalary;
-    }
-
-    public void setJobSalary(Double jobSalary) {
-        this.jobSalary = jobSalary;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public boolean isApproved() {
-        return isApproved;
-    }
-
-    public void setApproved(boolean approved) {
-        isApproved = approved;
-    }
+    Integer getId();
+    boolean isApproved();
+    Integer getUserId();
+    String getEmail();
+    String getJobTitle();
+    String getJobDescription();
+    Double getJobSalary();
+    Date getCreatedAt();
+    Date getUpdatedAt();
+    Integer getJobId();
 }
