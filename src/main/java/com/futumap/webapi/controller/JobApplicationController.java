360bf908717a836fcb1dda70b37cf477c8198d6f
Andreas Plado <andreasplado@gmail.com>
2020-09-13 16:58:04 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/controller/JobApplicationController.java
+++ src/main/java/com/futumap/webapi/controller/JobApplicationController.java
@@ -55,7 +55,7 @@ public class JobApplicationController {
 
     @RequestMapping(value = "/apply", method = RequestMethod.POST)
     public ResponseEntity<ResponseModel> apply(@RequestParam Integer userId, @RequestParam Integer applyerId) {
-        //jobApplicationService.deleteJobApplications(userId);
+        jobApplicationService.deleteJobApplications(userId);
         jobApplicationService.update(applyerId, userId);
         ResponseModel responseModel = new ResponseModel();
         responseModel.setMessage("You applied to job!");
