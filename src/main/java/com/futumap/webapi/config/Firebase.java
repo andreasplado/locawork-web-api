eb4e3efde9020a2acb25e14fd64b3b0054557996
Andreas Plado <andreasplado@gmail.com>
2020-09-24 22:59:00 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/config/Firebase.java
+++ src/main/java/com/futumap/webapi/config/Firebase.java
@@ -5,11 +5,23 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
 
+@Configuration
 public class Firebase {
-
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource("firebase-service-account.json").getInputStream());
+        FirebaseOptions firebaseOptions = FirebaseOptions
+                .builder()
+                .setCredentials(googleCredentials)
+                .build();
+        FirebaseApp app = FirebaseApp.initializeApp(firebaseOptions, "futumap");
+        return FirebaseMessaging.getInstance(app);
+    }
 
 }
