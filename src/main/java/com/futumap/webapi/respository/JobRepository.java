8ba36d677d55853bcf324d2ef0dee27cf6943ba7
Andreas Plado <andreasplado@gmail.com>
2020-10-01 17:01:52 +0300

    Refactoring

--- src/main/java/com/futumap/webapi/respository/JobRepository.java
+++ src/main/java/com/futumap/webapi/respository/JobRepository.java
@@ -15,28 +15,20 @@ public interface JobRepository extends JpaRepository<JobEntity, Integer> {
 
     @Query(value="SELECT j.*, u.* from jobs j " +
             "INNER JOIN users u ON j.user_id = u.id " +
-            "WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND j.applyer_id IS NULL AND u.id!=?4", nativeQuery = true)
+            "WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND j.applyer_id IS NULL AND u.id!=?4 AND j.is_done=false", nativeQuery = true)
     List<JobEntity> findOtherUsersNearestJobs(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("distance") Double distance, @Param("userId") Integer userId);
 
     @Query(value="SELECT j.*, u.* from jobs j " +
             "INNER JOIN users u ON j.user_id = u.id " +
-            "WHERE u.id=?1", nativeQuery = true)
+            "WHERE u.id=?1 AND j.is_done=false", nativeQuery = true)
     List<JobEntity> findPostedJobs(@Param("userId") Integer userId);
 
     @Query(value="SELECT j.*, u.* from jobs j " +
             "INNER JOIN users u ON j.user_id = u.id " +
-            "WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND j.applyer_id IS NULL", nativeQuery = true)
+            "WHERE earth_box(ll_to_earth(?1,?2),?3) @> ll_to_earth(j.latitude,j.longitude) AND j.applyer_id IS NULL AND j.is_done=false", nativeQuery = true)
     List<JobEntity> findAllNearestJobs(@Param("latitude") Double latitude, @Param("longitude") Double longitude, @Param("distance") Double distance);
 
     @Query(value="SELECT j.* from jobs j " +
-            "WHERE j.applyer_id=?1", nativeQuery = true)
+            "WHERE j.applyer_id=?1 AND j.is_done=false", nativeQuery = true)
     List<JobEntity> findByUserId(@Param("userId") Integer userId);
-
-    @Query(value="SELECT j.* from jobs j " +
-            "WHERE j.id=?1", nativeQuery = true)
-    JobEntity findSingleById(Integer id);
-
-    @Query(value="SELECT j.* from jobs j " +
-            "WHERE j.applyer_id=?1", nativeQuery = true)
-    List<JobEntity> findDoneWorkByUserId(@Param("userId") Integer userId);
 }
