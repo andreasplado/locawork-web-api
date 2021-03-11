186fea2a1b6454d507e939a9ef37c69f66f9b21d
Andreas Plado <andreasplado@gmail.com>
2021-03-11 02:51:11 +0200

    Refactoring

--- src/main/java/com/locawork/webapi/config/firebase/FCMInitializer.java
+++ src/main/java/com/locawork/webapi/config/firebase/FCMInitializer.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ import java.io.IOException;
 @Service
 public class FCMInitializer {
 
-    @Value("${app.firebase-configuration-file}")
+    @Value("${app.firebase-config}")
     private String firebaseConfigPath;
 
     Logger logger = LoggerFactory.getLogger(FCMInitializer.class);
