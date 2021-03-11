f5cb00deb62b620bb458dfa06193c9fd47cd660a
Andreas Plado <andreasplado@gmail.com>
2021-03-11 08:37:33 +0200

    Refactoring

--- src/main/java/com/locawork/webapi/dto/JobDTO.java
+++ src/main/java/com/locawork/webapi/dto/JobDTO.java
@@ -3,108 +3,26 @@ package com.locawork.webapi.dto;
 import javax.persistence.Column;
 import java.util.Date;
 
-public class JobDTO {
+public interface JobDTO {
 
-    private String title;
+    String title();
 
-    private Integer userId;
+    Integer userId();
 
-    private String description;
+    String description();
 
-    private Integer categoryId;
+    Integer categoryId();
 
-    private Double salary;
+    Double salary();
 
-    private Double latitude;
+    Double latitude();
 
-    private Double longitude;
+    Double longitude();
 
-    private Date createdAt;
+    Date createdAt();
 
-    private boolean isDone;
+    boolean isDone();
 
-    private String firebase_token;
+    String firebase_token();
 
-    public JobDTO() {
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
-
-    public Double getLatitude() {
-        return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
-    }
-
-    public Double getLongitude() {
-        return longitude;
-    }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }
-
-    public Date getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public boolean isDone() {
-        return isDone;
-    }
-
-    public void setDone(boolean done) {
-        isDone = done;
-    }
-
-    public String getFirebase_token() {
-        return firebase_token;
-    }
-
-    public void setFirebase_token(String firebase_token) {
-        this.firebase_token = firebase_token;
-    }
 }
