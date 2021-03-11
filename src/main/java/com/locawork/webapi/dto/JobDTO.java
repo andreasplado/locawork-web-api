b2ba752156b01a071a3445efbba927f91209b208
Andreas Plado <andreasplado@gmail.com>
2021-03-11 08:22:17 +0200

    Refactoring

---
+++ src/main/java/com/locawork/webapi/dto/JobDTO.java
@@ -0,0 +1,110 @@
+package com.locawork.webapi.dto;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+public class JobDTO {
+
+    private String title;
+
+    private Integer userId;
+
+    private String description;
+
+    private Integer categoryId;
+
+    private Double salary;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private Date createdAt;
+
+    private boolean isDone;
+
+    private String firebase_token;
+
+    public JobDTO() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+
+    public String getFirebase_token() {
+        return firebase_token;
+    }
+
+    public void setFirebase_token(String firebase_token) {
+        this.firebase_token = firebase_token;
+    }
+}
