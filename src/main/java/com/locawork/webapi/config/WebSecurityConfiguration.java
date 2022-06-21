0aaa833419b69b8fc2b1346699217f915f7666cf
Andreas Plado <andreasplado@gmail.com>
2022-06-21 03:06:20 +0300

    Refactoring

--- src/main/java/com/locawork/webapi/config/WebSecurityConfiguration.java
+++ src/main/java/com/locawork/webapi/config/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package com.locawork.webapi.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +12,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-@Configuration
+
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private BCryptPasswordEncoder bCryptPasswordEncoder;
